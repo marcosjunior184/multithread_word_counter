d1ce27129f2797a7ee3617bd191273384b69e808
Marcos da Silva Junior <dasilvam@esti.ca>
2024-04-19 17:00:59 -0600

    Add  header for class to hold task to be processed after the workers has finished their task and create cpp file.

---
+++ C++/Computation_Queue.hpp
@@ -0,0 +1,36 @@
+#ifndef _COMPUTATION_QUEUE_H_
+#define _COMPUTATION_QUEUE_H_
+
+#include <condition_variable>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <mutex> 
+#include <queue> 
+#include <map>
+
+
+namespace Computation
+{
+    class Computation_Queue
+    {
+        public:
+            struct Comp_Unit{
+                std::map<std::string, int> task_;
+                bool is_sentinel;
+            };
+
+
+            Computation_Queue();
+            void put(Comp_Unit unit);
+            Comp_Unit get();
+
+        private:
+            std::queue<std::map<std::string, int>> task_queue_;
+            std::mutex queue_mutex_;
+            std::condition_variable cv_;  
+
+    }
+}
+
+#endif
